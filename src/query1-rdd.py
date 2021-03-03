a9d04f1ebffedfce3d8b00e0fc6dfd87453f205b
manos <arapidismanolis@gmail.com>
2021-03-03 06:12:35 +0200

    added 1st query rdd

---
+++ src/query1-rdd.py
@@ -0,0 +1,35 @@
+#!/bin/python3
+# import pandas as pd 
+import logging
+import pyspark.sql.functions as f
+from pyspark.sql import SparkSession
+from io import StringIO
+import csv
+
+def split_complex(x):
+    return list(csv.reader(StringIO(x), delimiter=','))[0]
+
+def mapper1(x):
+    title=x[1]
+    year=x[3].split("-")[0]
+    cost=int(x[5])
+    revenue=int(x[6])
+    profit=((revenue-cost)/cost)*100
+    return [year,(title,profit)]
+
+def filter1(x):
+    year=x[3].split("-")[0]
+    cost=x[5]
+    revenue=x[6]
+    if year=="" or cost=='0' or revenue=='0':
+        return False
+    return int(year)>=2000
+
+spark = SparkSession.builder.appName("query1-rdd").getOrCreate()
+sc = spark.sparkContext
+
+rdd = sc.textFile('hdfs://master:9000/movie_data/movies.csv')
+
+rdd = rdd.map(split_complex).filter(filter1).map(mapper1).reduceByKey(lambda x,y: max((x, y), key=lambda x: x[1])).map(lambda x:(x[0],x[1][0])).sortByKey()
+
+print(rdd.take(50))
