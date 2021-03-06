a85e925553ab6aa8b46086979c109bf4bbbad0a7
manos <arapidismanolis@gmail.com>
2021-03-06 19:27:24 +0200

    unfinished

---
+++ src/query3-rdd.py
@@ -0,0 +1,55 @@
+#!/bin/python3
+from pyspark.sql import SparkSession
+from io import StringIO
+import csv
+
+def mapper1(x):
+    tokens=x.split(",")
+    _id=int(tokens[1])
+    rating=float(tokens[2])
+    return(_id,rating)
+
+def mapper2(x):
+    tokens=x.split(",")
+    _id=int(tokens[0])
+    genre=tokens[1]
+    return (_id,genre)
+
+def filter1(x):
+    genre = x[1][0]
+    if genre=="Drama":
+        return True
+    return False
+
+def map_distinct(x):
+    tokens=x.split(",")
+    # _id=int(tokens[0])
+    genre=tokens[1]
+    return (genre)
+
+spark = SparkSession.builder.appName("query3-rdd").getOrCreate()
+sc = spark.sparkContext
+
+rating = sc.textFile('hdfs://master:9000/movie_data/ratings.csv')
+genres = sc.textFile('hdfs://master:9000/movie_data/movie_genres.csv')
+
+rating_t = rating.map(mapper1)
+genres_t = genres.map(mapper2)
+
+# distinct = genres.map(map_distinct).countByValue()
+
+
+joined = genres_t.join(rating_t)
+distinct = joined.map(lambda x: (x[0],x[1][0])).distinct()
+# map(lambda x: x[1][0]).countByValue()
+print(distinct)
+# print(joined.take(30))
+# print(joined.count())
+
+# output = joined.map(lambda x: (x[1][0],x[0],x[1][1]))
+# .groupByKey().map()
+# .reduceByKey(lambda x,y: (x[0]+y[0],x[1]+y[1]))
+# .map(lambda x: (x[0],x[1][0],x[1][1]/x[1][0]))
+# .reduceByKey(lambda x,y : x)
+
+# print(output.take(25))
---
+++ src/test.py
@@ -0,0 +1,42 @@
+#!/bin/python3
+
+from pyspark.sql import SparkSession
+from io import StringIO
+import csv
+spark = SparkSession.builder.appName("query3-sql").getOrCreate()
+
+ratings_csv = spark.read.csv('hdfs://master:9000/movie_data/ratings.csv',inferSchema='true')
+movies_genres_csv = spark.read.csv('hdfs://master:9000/movie_data/movie_genres.csv',inferSchema='true')
+
+ratings_csv.registerTempTable("ratings_csv")
+movies_genres_csv.registerTempTable("movies_genres_csv")
+
+ratings_csv.printSchema()
+movies_genres_csv.printSchema()
+
+spark.sql(" select count(*) as Drama_num\
+             from \
+            (select _c0 as id, _c1 as genres \
+            from movies_genres_csv) \
+            join \
+            (select _c1 as id, _c2 as rating \
+            from ratings_csv) \
+            using(id)\
+            where genres='Drama'").show()
+
+# ratings_parquet = spark.read.load('hdfs://master:9000/movie_data/ratings.parquet')
+# movies_genres_parquet = spark.read.load('hdfs://master:9000/movie_data/movie_genres.parquet')
+
+# ratings_csv.registerTempTable("ratings_parquet")
+# movies_genres_csv.registerTempTable("movies_genres_parquet")
+
+
+# spark.sql(" select genres, count(distinct id) as total, avg(rating) as mean_rating\
+#              from \
+#             (select _c0 as id, _c1 as genres \
+#             from movies_genres_parquet) \
+#             join \
+#             (select _c1 as id, _c2 as rating \
+#             from ratings_parquet) \
+#             using(id)\
+#             group by genres").show()
